8408128ec37ec689dcf8457b9acd490f637a2090
AshersLab <asher.parry@icloud.com>
2021-07-18 00:53:43 +1000

    try this?

--- LDTTeam.Authentication.Server/Startup.cs
+++ LDTTeam.Authentication.Server/Startup.cs
@@ -87,7 +87,11 @@ namespace LDTTeam.Authentication.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders();
+            app.UseForwardedHeaders(
+                new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedProto
+                });
             
             if (false)//env.IsDevelopment())
             {
