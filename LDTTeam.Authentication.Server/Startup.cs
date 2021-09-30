26b7287999be5564ee9eb536099ac389551bfb94
AshersLab <asher.parry@icloud.com>
2021-09-30 13:55:08 +1000

    fix TLS behind reverse proxy

--- LDTTeam.Authentication.Server/Startup.cs
+++ LDTTeam.Authentication.Server/Startup.cs
@@ -122,8 +122,6 @@ namespace LDTTeam.Authentication.Server
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             LetsEncryptConfig? config = Configuration.GetSection("LetsEncrypt").Get<LetsEncryptConfig>();
@@ -133,7 +131,6 @@ namespace LDTTeam.Authentication.Server
                 app.UseFluffySpoonLetsEncrypt();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
