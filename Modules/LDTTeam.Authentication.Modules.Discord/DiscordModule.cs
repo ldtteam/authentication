c156f2a385bc4c8a3ec95bdbbc829632ea4b644a
Asher Parry <Asher.Parry@qube.com.au>
2025-04-17 22:52:49 +1000

    sync discord roles every 15 minutes, not only when patreon updated. Catches rare role issues

--- Modules/LDTTeam.Authentication.Modules.Discord/DiscordModule.cs
+++ Modules/LDTTeam.Authentication.Modules.Discord/DiscordModule.cs
@@ -27,8 +27,10 @@ namespace LDTTeam.Authentication.Modules.Discord
     {
         public string ModuleName => "Discord";
 
-        public AuthenticationBuilder ConfigureAuthentication(IConfiguration configuration,
-            AuthenticationBuilder builder)
+        public AuthenticationBuilder ConfigureAuthentication(
+            IConfiguration        configuration,
+            AuthenticationBuilder builder
+        )
         {
             DiscordConfig? discordConfig = configuration.GetSection("discord").Get<DiscordConfig>();
 
@@ -53,6 +55,8 @@ namespace LDTTeam.Authentication.Modules.Discord
 
             return services.AddHostedService<WebhookLoggingQueueService>()
                 .AddHostedService<DiscordBackgroundService>()
+                .AddHostedService<DiscordSyncRolesBackgroundService>()
+                .AddScoped<DiscordRoleSyncService>()
                 .AddStartupTask<DiscordStartupTask>()
                 .AddDiscordGateway(_ => discordConfig.BotToken)
                 .AddDiscordCommands(true)
@@ -65,133 +69,8 @@ namespace LDTTeam.Authentication.Modules.Discord
         public void EventsSubscription(IServiceProvider services, EventsService events, CancellationToken token)
         {
             events.PostRefreshContentEvent += async sp =>
-            {
-                IConditionService conditionService = sp.ServiceProvider.GetRequiredService<IConditionService>();
-                Dictionary<string, List<string>> rewards = await conditionService.GetRewardsForProvider("discord", token);
-                IDiscordRestGuildAPI guildApi = sp.ServiceProvider.GetRequiredService<IDiscordRestGuildAPI>();
-                IConfiguration configuration = sp.ServiceProvider.GetRequiredService<IConfiguration>();
-
-                DiscordConfig? discordConfig = configuration.GetSection("discord").Get<DiscordConfig>();
-
-                if (discordConfig == null)
-                    throw new Exception("discord not set in configuration!");
-
-                foreach (string server in discordConfig.RoleMappings.Keys)
-                {
-                    if (!ulong.TryParse(server, out ulong serverId)) continue;
-
-                    Snowflake serverSnowflake = new(serverId);
-
-                    Result<IGuild> guildResult = await guildApi.GetGuildAsync(serverSnowflake, true);
-                    if (!guildResult.IsSuccess || guildResult.Entity == null) continue;
-
-                    Result<IReadOnlyList<IRole>> rolesResult = await guildApi.GetGuildRolesAsync(serverSnowflake);
-                    if (!rolesResult.IsSuccess || rolesResult.Entity == null) continue;
-
-                    List<IGuildMember> members = new();
-
-                    bool errored = false;
-                    // ReSharper disable once PossibleLossOfFraction
-                    for (int i = 0;
-                        i <= Math.Ceiling((double) (guildResult.Entity.ApproximateMemberCount.Value / 1000));
-                        i++)
-                    {
-                        Optional<Snowflake> snowflake;
-                        if (members.LastOrDefault()?.User.HasValue == true &&
-                            members.LastOrDefault()?.User.Value.ID != null)
-                            snowflake = new Optional<Snowflake>(members.LastOrDefault()!.User.Value.ID);
-                        else
-                            snowflake = new Optional<Snowflake>();
-
-                        Result<IReadOnlyList<IGuildMember>> membersResult =
-                            await guildApi.ListGuildMembersAsync(
-                                serverSnowflake,
-                                1000, snowflake);
-
-                        if (!membersResult.IsSuccess || membersResult.Entity == null)
-                        {
-                            errored = true;
-                            break;
-                        }
-
-                        members.AddRange(membersResult.Entity);
-                    }
-
-                    if (errored) continue;
-
-                    Dictionary<string, List<Snowflake>> rewardRoles = discordConfig.RoleMappings[server]
-                        .ToDictionary(
-                            x => x.Key,
-                            x => x.Value.Select(y => new Snowflake(y)).ToList()
-                        );
-
-                    Dictionary<IGuildMember, IReadOnlyList<Snowflake>> memberRoles =
-                        members.ToDictionary(
-                            x => x,
-                            x => x.Roles
-                        );
-
-                    foreach ((IGuildMember user, IReadOnlyList<Snowflake> roles) in memberRoles)
-                    {
-                        if (user.User.HasValue == false) continue;
-
-                        Snowflake userSnowflake = user.User.Value.ID;
-
-                        List<string> userRewards = rewards
-                            .Where(x => x.Value.Contains(userSnowflake.ToString()))
-                            .Select(x => x.Key)
-                            .ToList();
-
-                        // roles to award
-                        List<Snowflake> rewardedRoles = rewardRoles
-                            .Where(x => userRewards.Contains(x.Key))
-                            .SelectMany(x => x.Value)
-                            .Distinct()
-                            .Select(x => x)
-                            .ToList();
-
-                        // roles not rewarded less rewardedRoles
-                        List<Snowflake> notRewardedRoles = rewardRoles
-                            .Where(x => !userRewards.Contains(x.Key))
-                            .SelectMany(x => x.Value)
-                            .Where(x => !rewardedRoles.Contains(x))
-                            .Distinct()
-                            .Select(x => x)
-                            .ToList();
-
-                        List<IRole> rolesToAdd =
-                            (from rewardRole in rewardedRoles
-                                let role = rolesResult.Entity.FirstOrDefault(x => x.ID == rewardRole)
-                                where !roles.Contains(rewardRole) && role != null
-                                select role).ToList()!;
-
-                        List<IRole> rolesToRemove =
-                            (from notRewardRole in notRewardedRoles
-                                let role = rolesResult.Entity.FirstOrDefault(x => x.ID == notRewardRole)
-                                where roles.Contains(notRewardRole) && role != null
-                                select role).ToList()!;
-                        
-                        // don't add roles if only optionals being added
-                        if (!rolesToAdd.All(x => discordConfig.OptionalRoles.Contains(x.ID.Value)))
-                        {
-                            foreach (IRole role in rolesToAdd)
-                            {
-                                await guildApi.AddGuildMemberRoleAsync(serverSnowflake, userSnowflake, role.ID,
-                                    "LDTTeam Auth user has rewards for this role");
-                            }
-                        }
-
-                        if (!discordConfig.RemoveUsersFromRoles ||
-                            discordConfig.UserExceptions.Contains(userSnowflake.Value)) continue;
-
-                        foreach (IRole role in rolesToRemove)
-                        {
-                            await guildApi.RemoveGuildMemberRoleAsync(serverSnowflake, userSnowflake, role.ID,
-                                "LDTTeam Auth user does not have rewards for this role");
-                        }
-                    }
-                }
-            };
+                await sp.ServiceProvider.GetRequiredService<DiscordRoleSyncService>()
+                    .RunSync(token);
         }
     }
 }
