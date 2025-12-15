86dc74fa271e63a20abb713da267252190e6f016
Marc Hermans <marc.hermans@ldtteam.com>
2025-12-15 20:22:27 +0100

    Register Discord slash commands

--- LDTTeam.Authentication.DiscordBot/Program.cs
+++ LDTTeam.Authentication.DiscordBot/Program.cs
@@ -2,12 +2,14 @@
 
 using JasperFx;
 using LDTTeam.Authentication.DiscordBot.Extensions;
+using LDTTeam.Authentication.DiscordBot.Service;
 using LDTTeam.Authentication.Utils.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Remora.Discord.Commands.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +39,16 @@ app.MigrateDatabase();
 var result = await app.RunJasperFxCommands(args);
 
 logger.LogWarning("Application has stopped.");
+
+logger.LogWarning("Registering Discord Slash Commands...");
+using var scope = app.Services.CreateScope();
+var serverProvider = scope.ServiceProvider.GetRequiredService<IServerProvider>();
+var server = await serverProvider.GetServerAsync();
+
+var slashService = scope.ServiceProvider.GetRequiredService<SlashService>();
+await slashService.UpdateSlashCommandsAsync();
+await slashService.UpdateSlashCommandsAsync(server.Id);
+logger.LogWarning("Discord Slash Commands registered.");
 
 return result;
 
