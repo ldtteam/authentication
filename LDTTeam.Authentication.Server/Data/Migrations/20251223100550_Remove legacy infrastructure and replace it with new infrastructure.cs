a635e9eb9101e737f9f40d7578f4f0844f37584f
Marc Hermans <marc.hermans@ldtteam.com>
2025-12-23 11:37:00 +0100

    Update to the new infrastructure

--- LDTTeam.Authentication.Server/Data/Migrations/20251223100550_Remove legacy infrastructure and replace it with new infrastructure.cs
+++ LDTTeam.Authentication.Server/Data/Migrations/20251223100550_Remove legacy infrastructure and replace it with new infrastructure.cs
@@ -25,11 +25,27 @@ namespace LDTTeam.Authentication.Server.Data.Migrations
                 {
                     table.PrimaryKey("PK_KnownRewards", x => new { x.Type, x.Name });
                 });
+            
+            migrationBuilder.CreateTable(
+                name: "AssignedRewards",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Reward = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignedRewards", x => new { x.UserId, x.Type, x.Reward });
+                });
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "AssignedRewards");
+            
             migrationBuilder.DropTable(
                 name: "KnownRewards");
 
