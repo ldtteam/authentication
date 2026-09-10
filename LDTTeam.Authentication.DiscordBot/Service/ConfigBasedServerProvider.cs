9d5c58b7886ebb2f4e7b70c1485ee30a78d79838
Marc Hermans <marc.hermans@ldtteam.com>
2026-09-10 21:42:22 +0200

    Revert "Update package references to latest versions across multiple projects"
    This reverts commit d3328b0c8cd621db94d3975e8dd6c1d1083670df.

--- LDTTeam.Authentication.DiscordBot/Service/ConfigBasedServerProvider.cs
+++ LDTTeam.Authentication.DiscordBot/Service/ConfigBasedServerProvider.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
 using LDTTeam.Authentication.DiscordBot.Config;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.Extensions.Options;
 using Remora.Rest.Core;
 
